5fb0c0d619444f0596e40669c1e8f3eb93ee83fd
Sawyer Mifsud <wingren013@gmail.com>
2018-11-09 17:18:24 -0800

    fixed body

--- includes/barneshut.h
+++ includes/barneshut.h
@@ -1,6 +1,22 @@
 #ifndef BARNESHUT
 # define BARNESHUT
 
+
+class Body
+{
+	//position;
+	double	x;
+	double	y;
+	double	z;
+
+	double	mass;
+
+	//velocity
+	double	vx;
+	double	vy;
+	double	vz;
+}
+
 class BarnesHutNode
 {
 	BarnesHutNode	*children[8];
