a3146923ba5994b81710c5fa7449a5c1ada85d4b
Sawyer Mifsud <wingren013@gmail.com>
2018-11-09 17:10:03 -0800

    added dummy bh node header

--- includes/barneshut.h
+++ includes/barneshut.h
@@ -6,17 +6,8 @@ class BarnesHutNode
 	BarnesHutNode	*children[8];
 	BarnesHutNode	*parent;
 	
-	//position
-	double		x;
-	double		y;
-	double		z;
-
-	double		mass;
-
-	//velocity
-	double		vx;
-	double		vy;
-	double		vz;
+	//stars
+	Body		*bodies;
 };
 
 #endif
